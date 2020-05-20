c850246d91992017565aec876949a6531a311e8e
himself65 <himself65@outlook.com>
2020-05-20 11:35:32 +0800

    app: update toast for bluetooth tip

--- app/src/main/java/com/himself65/raspberrypiconnector/MainActivity.kt
+++ app/src/main/java/com/himself65/raspberrypiconnector/MainActivity.kt
@@ -1,22 +1,20 @@
 package com.himself65.raspberrypiconnector
 
+import android.app.Activity
 import android.bluetooth.BluetoothAdapter
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.view.MenuItem
 import android.view.View
 import android.widget.PopupMenu
+import android.widget.Toast
+
+const val REQUEST_ENABLE_BT: Int = 1
 
 class MainActivity : AppCompatActivity() {
     private var bluetoothAdapter: BluetoothAdapter? = null
     private var popupMenu: PopupMenu? = null
-
-    companion object {
-        @JvmStatic
-        var REQUEST_ENABLE_BT: Int = 1
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,13 +26,32 @@ class MainActivity : AppCompatActivity() {
             Log.i("BLUETOOTH", "Support Bluetooth")
             if (!this.bluetoothAdapter!!.isEnabled) {
                 // request to enable bluetooth
-                val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
-                startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT)
+                Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE).also { intent ->
+                    startActivityForResult(intent, REQUEST_ENABLE_BT)
+                }
             }
         }
     }
 
-    final fun onMainButtonClick(view: View) {
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_ENABLE_BT) {
+            if (resultCode != Activity.RESULT_OK) {
+                Toast.makeText(
+                    this,
+                    "Failed to turn on the bluetooth.\n" +
+                            "Please turn it on to make sure the app works well.",
+                    Toast.LENGTH_SHORT
+                )
+                    .also { toast ->
+                        Log.i("${toast.view.id}", "Not turn on bluetooth")
+                    }
+                    .show()
+            }
+        }
+    }
+
+    fun onMainButtonClick(view: View) {
         val popupMenu: PopupMenu =
             if (this.popupMenu == null) PopupMenu(this, view) else this.popupMenu as PopupMenu
         if (this.popupMenu == null) {
@@ -49,9 +66,8 @@ class MainActivity : AppCompatActivity() {
                 // todo: send message to target address
                 true
             }
+            this.popupMenu = popupMenu
         }
         popupMenu.show()
-
-        this.popupMenu = popupMenu
     }
 }
